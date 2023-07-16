bfac6e20eed69955358a26a1d2dc97336ab63e65
Colin Campbell <142777+ColinCampbell@users.noreply.github.com>
2023-07-15 23:40:39 -0700

    bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ import setuptools
 if __name__ == "__main__":
     setuptools.setup(
         name="decora_bleak",
-        version="0.0.1"
+        version="0.1.0"
     )
--- src/decora_bleak/__init__.py
+++ src/decora_bleak/__init__.py
@@ -6,7 +6,7 @@
 # MIT license
 # This code was inspired by https://github.com/mjg59/python-decora and https://github.com/lucapinello/pydecora_ble/tree/master
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 from .decora_bleak import DecoraBLEDevice
 from .const import DECORA_SERVICE_UUID
